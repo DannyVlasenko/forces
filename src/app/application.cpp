00b3ab3e479cde66ad3371847f1038182b3078fe
Daniil Vlasenko <daniil.vlasenko@paradoxcat.com>
2022-08-11 18:07:12 +0200

    camera rotation

--- src/app/application.cpp
+++ src/app/application.cpp
@@ -66,10 +66,10 @@ namespace forces
 		mMainWindow.set_swap_interval(1);
 
 		constexpr float positions[] = {
-			250.f, 250.f,
-			500.f, 250.f,
-			500.f, 500.f,
-			250.f, 500.f
+			-25.f, -25.f,
+			25.f, -25.f,
+			25.f, 25.f,
+			-25.f, 25.f
 		};
 
 		constexpr GLuint indices[] = {
@@ -86,10 +86,17 @@ namespace forces
 		opengl::IndexBuffer ib(indices);
 
 		auto wndSize = mMainWindow.size();
-		glm::mat4 proj = glm::ortho(0.f, wndSize.x, 0.f, wndSize.y, -1.0f, 1.0f);
+		//glm::mat4 proj = glm::ortho(0.f, wndSize.x, 0.f, wndSize.y, -1.0f, 1.0f);
+
+		auto proj = glm::perspective(glm::radians(60.f), wndSize.x / wndSize.y, 0.1f, 200.f);
+		auto view = glm::lookAt(glm::vec3(0.f, 0.f, -100.f),
+								glm::vec3(0,0,0),
+								glm::vec3(0,1,0));
+		auto model = glm::mat4(1.f);
+		model = glm::rotate(model, glm::radians(30.f), glm::vec3(0.f, 1.f, 0.f));
 
 		opengl::Program colorProgram{ opengl::Shader{ VertexSrc }, opengl::Shader{ FragmentSrc } };
-		colorProgram.set_uniform_mat4("u_MVP", proj);
+		colorProgram.set_uniform_mat4("u_MVP", proj * view * model);
 		colorProgram.set_uniform_4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 
 		opengl::Program::unbind();
@@ -101,15 +108,26 @@ namespace forces
 		float r = 0.0f;
 		float increment = 0.05f;
 
+		float grad = 0;
+
 		mMainLoop.run([&]
 		{
 			GLCall(glClear(GL_COLOR_BUFFER_BIT));
-			auto wndSize = mMainWindow.size();
-			proj = glm::ortho(0.f, wndSize.x, 0.f, wndSize.y, -1.0f, 1.0f);
-			colorProgram.set_uniform_mat4("u_MVP", proj);
+			colorProgram.set_uniform_mat4("u_MVP", proj * view * model);
 			colorProgram.set_uniform_4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
 			renderer.draw(va, ib, colorProgram);
+			auto y = 100 * glm::sin(glm::radians(grad));
+			auto z = 100 * glm::cos(glm::radians(grad));
+			grad += 0.5f;
+			if (grad >= 360.f)
+			{
+				grad = 0.0f;
+			}
+
+			view = glm::lookAt(glm::vec3(0.f, y, z),
+				glm::vec3(0, 0, 0),
+				glm::vec3(0, 1, 0));
 
 			if (r < 0.0f || r > 1.0f)
 			{
