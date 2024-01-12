6e9a034b22783256196d26b0d47bcd23eb21169b
Daniil Vlasenko <danny.vlasenko@gmail.com>
2024-01-12 18:35:38 +0100

    SceneViewController code moved to window class

--- projects/vs/extension/Windows/SceneViewWindow.cs
+++ projects/vs/extension/Windows/SceneViewWindow.cs
@@ -1,39 +1,97 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using Forces.Engine;
 using Forces.Models;
+using Forces.ViewModels;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Forces.Windows
 {
-	/// <summary>
-	/// This class implements the tool window exposed by this package and hosts a user control.
-	/// </summary>
-	/// <remarks>
-	/// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
-	/// usually implemented by the package implementer.
-	/// <para>
-	/// This class derives from the ToolWindowPane class provided from the MPF in order to use its
-	/// implementation of the IVsUIElementPane interface.
-	/// </para>
-	/// </remarks>
 	[Guid("606da3ec-a27c-4e36-b3fd-4d13f23204df")]
-	public class SceneViewWindow : ToolWindowPane
+	public sealed class SceneViewWindow : ToolWindowPane
 	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SceneViewWindow"/> class.
-		/// </summary>
+		private readonly SelectedSceneModel _model;
+		private IVsWindowFrame _frame = null;
+		private readonly SceneViewWindowControl _control;
+		private Scene _currentScene;
+
+		private string Title => "Forces Scene - " + ((_model?.SceneName?.Length ?? 0) == 0 ? "No Scene Selected" : _model.SceneName);
 		public SceneViewWindow(SelectedSceneModel model) : base(null)
 		{
-			this.Caption = "SceneViewWindow";
+			_model = model;
+			_control = new SceneViewWindowControl();
+			_control.SelectedItemChanged += _control_SelectedItemChanged;
+			Content = _control;
+			Caption = Title;
 
-			// This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
-			// we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
-			// the object returned by the Content property.
-			this.Content = new SceneViewWindowControl(this, model);
+			if (_model.SelectedScene?.RootNode != null)
+			{
+				_currentScene = _model.SelectedScene;
+				_control.AddItem(new SceneViewNode(_model.SelectedScene.RootNode));
+				_control.AddItem(new SceneViewCamera(_model.SelectedScene.PreviewCamera));
+				TrackSelection(_control.SelectedItem);
+			}
+
+			_model.SelectedSceneChanged += _model_SelectedSceneChanged;
 		}
-		internal object GetVsService(Type service)
+
+		private void _control_SelectedItemChanged(object sender, ISceneViewNode e)
 		{
-			return GetService(service);
+			if (e is SceneViewNode svn)
+			{
+				TrackSelection(new NodePropertiesModel(svn.Node, _model));
+			}
+			if (e is SceneViewCamera svc)
+			{
+				TrackSelection(new CameraPropertiesModel(svc.Camera, _model));
+			}
+		}
+
+		private void _model_SelectedSceneChanged(object sender, Engine.Scene e)
+		{
+			if (_currentScene == e)
+			{
+				return;
+			}
+			Caption = Title;
+			_control.ClearItems();
+			if (_model.SelectedScene?.RootNode != null)
+			{
+				_currentScene = _model.SelectedScene;
+				_control.AddItem(new SceneViewNode(_model.SelectedScene.RootNode));
+				_control.AddItem(new SceneViewCamera(_model.SelectedScene.PreviewCamera));
+				TrackSelection(_control.SelectedItem);
+			}
+		}
+
+		private void TrackSelection(object selection)
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+			if (_frame == null)
+			{
+				if (GetService(typeof(SVsUIShell)) is IVsUIShell shell)
+				{
+					var guidPropertyBrowser = new Guid(ToolWindowGuids.PropertyBrowser);
+					shell.FindToolWindow((uint)__VSFINDTOOLWIN.FTW_fForceCreate, ref guidPropertyBrowser, out _frame);
+				}
+			}
+
+			_frame?.Show();
+			var mySelContainer = new SelectionContainer()
+			{
+				SelectedObjects = new System.Collections.ArrayList()
+				{
+					selection
+				}
+			};
+
+			if (GetService(typeof(STrackSelection)) is ITrackSelection track)
+			{
+				track.OnSelectChange(mySelContainer);
+			}
 		}
 	}
 }
--- projects/vs/extension/Windows/SceneViewWindowControl.xaml.cs
+++ projects/vs/extension/Windows/SceneViewWindowControl.xaml.cs
@@ -1,132 +1,40 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using Forces.Engine;
-using Forces.Models;
 using Forces.ViewModels;
-using Forces.Windows;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell;
 
-namespace Forces
+namespace Forces.Windows
 {
-	/// <summary>
-	/// Interaction logic for SceneViewWindowControl.
-	/// </summary>
 	public partial class SceneViewWindowControl : UserControl
 	{
-
-		private readonly SceneViewWindow _parent;
-		private readonly SelectedSceneModel _sceneModel;
-		private Scene _currentScene;
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SceneViewWindowControl"/> class.
-		/// </summary>
-		public SceneViewWindowControl(SceneViewWindow parent, SelectedSceneModel sceneModel)
+		public SceneViewWindowControl()
 		{
-			_parent = parent;
-			_sceneModel = sceneModel;
 			this.InitializeComponent();
-			_sceneModel.SelectedSceneChanged += _sceneModel_SelectedSceneChanged;
-			if (_sceneModel.SelectedScene?.RootNode != null)
-			{
-				_currentScene = _sceneModel.SelectedScene;
-				SceneTreeView.Items.Add(new ViewModels.SceneViewNode(_sceneModel.SelectedScene.RootNode));
-				SceneTreeView.Items.Add(new ViewModels.SceneViewCamera(_sceneModel.SelectedScene.PreviewCamera));
-				TrackSelection();
-			}
 		}
 
-		private void _sceneModel_SelectedSceneChanged(object sender, Scene e)
+		public IEnumerable<ISceneViewNode> Items => SceneTreeView.Items.OfType<ISceneViewNode>();
+
+		public ISceneViewNode SelectedItem => SceneTreeView.SelectedItem as ISceneViewNode;
+
+		public void AddItem(ISceneViewNode item)
 		{
-			if (_currentScene == e)
-			{
-				return;
-			}
-
-			SceneTreeView.Items.Clear();
-			if (_sceneModel.SelectedScene?.RootNode != null)
-			{
-				_currentScene = _sceneModel.SelectedScene;
-				SceneTreeView.Items.Add(new ViewModels.SceneViewNode(_sceneModel.SelectedScene.RootNode));
-				SceneTreeView.Items.Add(new ViewModels.SceneViewCamera(_sceneModel.SelectedScene.PreviewCamera));
-				TrackSelection();
-			}
+			SceneTreeView.Items.Add(item);
 		}
 
-		/// <summary>
-		/// Handles click on the button by displaying a message box.
-		/// </summary>
-		/// <param name="sender">The event sender.</param>
-		/// <param name="e">The event args.</param>
-		[SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-		[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-		private void button1_Click(object sender, RoutedEventArgs e)
-		{
-			ThreadHelper.ThrowIfNotOnUIThread();
-			if (textBox1.Text.Length > 0)
-			{
-				//var item = new Node(textBox1.Text);
-				//item.Children.Add(new Node(textBox1.Text + "_child"));
-				//item.Children[0].Children.Add(new Node(textBox1.Text + "_subchild"));
-				//SceneTreeView.Items.Add(item);
-				//var outputWindow = (IVsOutputWindow)_parent.GetVsService(typeof(SVsOutputWindow));
-				//var guidGeneralPane = VSConstants.GUID_OutWindowGeneralPane;
-				//outputWindow.GetPane(ref guidGeneralPane, out var pane);
-				//pane?.OutputStringThreadSafe($"Node created: {item}\r\n");
-				//TrackSelection();
-				////CheckForErrors();
-			}
-		}
-
-		private SelectionContainer _mySelContainer;
-		private System.Collections.ArrayList _mySelItems;
-		private IVsWindowFrame _frame = null;
-
-		private void TrackSelection()
-		{
-			ThreadHelper.ThrowIfNotOnUIThread();
-			if (_frame == null)
-			{
-				if (_parent.GetVsService(typeof(SVsUIShell)) is IVsUIShell shell)
-				{
-					var guidPropertyBrowser = new Guid(ToolWindowGuids.PropertyBrowser);
-					shell.FindToolWindow((uint)__VSFINDTOOLWIN.FTW_fForceCreate, ref guidPropertyBrowser, out _frame);
-				}
-			}
-
-			_frame?.Show();
-			if (_mySelContainer == null)
-			{
-				_mySelContainer = new SelectionContainer();
-			}
-
-			_mySelItems = new System.Collections.ArrayList();
-
-			if (SceneTreeView.SelectedItem is SceneViewNode node)
-			{
-				_mySelItems.Add(new NodePropertiesModel(node.Node, _sceneModel));
-			}
-
-			if (SceneTreeView.SelectedItem is SceneViewCamera camera)
-			{
-				_mySelItems.Add(new CameraPropertiesModel(camera.Camera, _sceneModel));
-			}
-
-			_mySelContainer.SelectedObjects = _mySelItems;
-
-			if (_parent.GetVsService(typeof(STrackSelection)) is ITrackSelection track)
-			{
-				track.OnSelectChange(_mySelContainer);
-			}
-		}
-
+		public event EventHandler<ISceneViewNode> SelectedItemChanged; 
 		private void SceneTreeView_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
 		{
-			TrackSelection();
+			if (SelectedItem != null)
+			{
+				SelectedItemChanged?.Invoke(this, SelectedItem);
+			}
+		}
+
+		public void ClearItems()
+		{
+			SceneTreeView.Items.Clear();
 		}
 	}
 }
